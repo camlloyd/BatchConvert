4114ebe84aaf7eb733b85df93a5349a74b522d6b
Cameron Lloyd <lloyd@med.unideb.hu>
2026-05-03 16:11:20 +0300

    Use explicit channels for Nextflow install

--- installation/install_with_nextflow.sh
+++ installation/install_with_nextflow.sh
@@ -11,8 +11,8 @@ chmod -R 777 $SCRIPTPATH;
 
 cd $SCRIPTPATH;
 
-conda create --yes --name nflow python=3.9 && \
-conda install --yes -n nflow -c bioconda nextflow==23.10.1 && \
+conda create --yes --name nflow -c conda-forge python=3.9 && \
+conda install --yes -n nflow -c conda-forge -c bioconda nextflow==23.10.1 && \
 mv nextflow.sample nextflow
 cd -
 
